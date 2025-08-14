cfa887a51553b06626aa9529f25d0d68c843965b
githubna-HasybiMiM <hasybimim@gmail.com>
2025-08-14 08:28:32 +0700

    ubah api

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:tefa_parkir/auth/auth.gate.dart';
 void main() async {
   // supabase setup
   await Supabase.initialize(
-    url: "https://cmtyyhsiuwmakwpvkdln.supabase.co", 
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtdHl5aHNpdXdtYWt3cHZrZGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyODc1ODMsImV4cCI6MjA2OTg2MzU4M30.E5g1HC9WKqRKtqt1N7YmoFmT-S5Ncfzmnb8GbBz1EkA",
+    url: "https://rfpsfzbmhhxksisxciwx.supabase.co", 
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmcHNmemJtaGh4a3Npc3hjaXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxMzM2MzMsImV4cCI6MjA3MDcwOTYzM30.OdBMWNBjgls2iw08JPqId9osfDTVE0W00H6zGHvOe_U",
   ); 
 
   runApp(const MyApp());
